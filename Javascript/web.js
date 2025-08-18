b558af7b2866a74f5fd3f1920afad992deb6fbe5
teggi-2002 <tejashwinisteggi2002@gmail.com>
2025-08-18 10:48:37 +0530

    Task:finally started with JS with the concept method iputs

---
+++ Javascript/web.js
@@ -0,0 +1,24 @@
+// Show an alert
+alert("helooooooo");
+
+// Ask user input
+prompt("Enter your amount");
+
+// Show confirmation
+confirm("Yes transaction done successfully");
+
+// Write something on the webpage
+document.writeln("Darshan<br>");
+
+// Log values in console
+console.log(2 + 2);
+console.log("darshan");
+
+// Show data types
+console.log(typeof console);
+
+// Correct spelling of prompt
+console.log(prompt("Enter your name"));
+
+// Confirm input
+console.log(confirm("Is that your name?"));
