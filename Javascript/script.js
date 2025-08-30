0dc841ce474c03a840ba4e99d259f8fa0a164d04
teggi-2002 <tejashwinisteggi2002@gmail.com>
2025-08-30 11:16:28 +0530

    Grid and Gridarea in CSS

---
+++ Javascript/script.js
@@ -0,0 +1,19 @@
+// var a;
+// a=10
+// a=20
+// var=100;
+
+var a="XYZ"
+let b=10
+const c=20
+function demo(){
+    var e=100
+    let f=200
+    const g=300
+    console.log(e,f,g);
+}
+console.log(a)
+console.log(b)
+console.log(c)
+
+demo()
